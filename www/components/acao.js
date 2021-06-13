0195df6f3cec0e8d77683356faeaf6bbd3ad5178
beatrizsibilio <beatrizduartesibilio@gmail.com>
2021-06-14 08:20:21 +0900

    atualizacao

---
+++ www/components/acao.js
@@ -0,0 +1,29 @@
+// This is a JavaScript file
+
+var num;
+
+$(document).on("click","#calcular", function(){
+  num = $('#numero').val();
+
+  valorInvalido = function(){
+    $("#resultado").html("<b>Nenhum número foi selecionado!</b>");
+    $("#resultado").css("border", "0px");
+  }
+
+  mostrarTabuada = function(){
+    var resultado, tabuada="";
+    for(var a = 1; a <= num; a++){
+      resultado = num * a;
+      tabuada += num + " X "+ a+ " = " + resultado + "<br>";
+    }
+    $("#resultado").html(tabuada);
+    $("#resultado").css("padding", "10px");
+    $("#resultado").css("border", "solid black 1px");
+  }
+
+  if(num == "") {
+    valorInvalido();
+  } else {
+    mostrarTabuada();
+  }
+});
